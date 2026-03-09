b82e2c181cc6a0b2e22b673b17eb8e5d98939466
Raul-Sentinel <raul.sentinel@sentinelbank.com.br>
2026-03-09 12:02:21 -0300

    test(test:run) Clase de execução de testes

---
+++ src/test/java/br/com/tecprimesolucoes/online_shop_system/OnlineShopSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tecprimesolucoes.online_shop_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
